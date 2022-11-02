4711fb429fa72def7149709ef7985883330c49a4
KalpasWang <wf6212@gmail.com>
2022-11-02 23:55:16 +0800

    navbar complete

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const menuContent = select('#menu-content');
 
 function handleDOMEvents() {
   select('#menu-button').addEventListener('click', (e) => {
-    menuContent.classList.toggle('max-h-full');
+    menuContent.classList.toggle('scale-y-100');
   });
   select('#close-button').addEventListener('click', (e) => {
-    menuContent.classList.remove('max-h-full');
+    menuContent.classList.remove('scale-y-100');
   });
 }
 
