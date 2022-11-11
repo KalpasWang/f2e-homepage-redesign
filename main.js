c92ded236def4379cd35a147dad88c874be8fc32
KalpasWang <wf6212@gmail.com>
2022-11-11 23:39:06 +0800

    temp

--- main.js
+++ main.js
@@ -1,18 +1,95 @@
 import "./style.css";
-import { gsap } from "gsap/all";
+import { gsap, ScrollTrigger } from "gsap/all";
+import Lenis from "@studio-freight/lenis";
+import SplitType from "split-type";
 
 const select = (e) => document.querySelector(e);
 
-const menuContent = select("#menu-content");
+gsap.registerPlugin(ScrollTrigger);
 
 function init() {
+  setSmoothScroll();
+  initOpeningAnimation();
   handleHeaderMenu();
   handleQnATabs();
 }
 
 init();
 
+function setSmoothScroll() {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
+
+function initOpeningAnimation() {
+  const hero = select("#hero");
+  const bigTitle = select("#big-title");
+  const subtitle = select("#subtitle");
+  const pieceCode = select("#piece1");
+  const pieceDashboard = select("#piece2");
+  const rightLine = select("#illustration1");
+  const leftLine = select("#illustration2");
+  const signupBtn = select("#signup-btn");
+  const marquee = select("#marquee");
+
+  window.scrollTo(0, 0);
+  document.body.classList.add("h-screen", "overflow-scroll");
+  const text = new SplitType(subtitle);
+
+  const t1 = gsap.timeline({
+    defaults: {
+      duration: 0.5,
+      ease: "power3.inOut",
+    },
+  });
+
+  t1.from(hero, {
+    yPercent: -100,
+    duration: 1.2,
+    autoAlpha: 0,
+  })
+    .from("header .container", {
+      autoAlpha: 0,
+    })
+    .from(bigTitle, {
+      yPercent: 25,
+      autoAlpha: 0,
+      ease: "none",
+    })
+    .fromTo(
+      "#subtitle .char",
+      {
+        yPercent: 100,
+      },
+      {
+        yPercent: 0,
+        stagger: 0.08,
+      },
+      "-=0.2"
+    );
+
+  document.body.classList.remove("h-screen", "overflow-scroll");
+}
+
 function handleHeaderMenu() {
+  const menuContent = select("#menu-content");
+
   select("#menu-button").addEventListener("click", (e) => {
     menuContent.classList.toggle("scale-y-100");
   });
